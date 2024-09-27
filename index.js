d65d1aa25c166c853b607927a9cd958bc7607a4a
sammou00 <sammoustafa00@gmail.com>
2024-09-27 14:11:55 +0200

    Update package.json

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
+// import middlewares
+import logger from './middleware/logger.js';
 
+// import routes
 
-
-// configure dotenv
+// load environment variables
 dotenv.config();
-const PORT = process.env.PORT || 5009;
+const PORT = process.env.PORT || 5003;
 
-
+// construct path
+const __filename = fileURLToPath(import.meta.url);
+const PATH = dirname(__filename);
 
 // initialize express
 const app = express();
 
-// parse body 
+// parses
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// serve static files
+app.use(express.static(path.join(PATH, 'public')));
+
+// set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(PATH, 'views'));
+
+// use middlewares
+app.use(logger);
 
 // use routes
 
-
 // handle 404
 app.use('*', (req, res) => {
-    res.status(404).json({ message: 'Page is not found' });
+    res.status(404).render('404', { title: '404', message: 'Page not found' });
 });
 
-// error
+// handle errors
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.error(err.stack);
+    res.status(500).render('404', {
+        title: '500',
+        message: 'Internal server error'
+    });
 });
-// listen
+
+// listen to port
 app.listen(PORT, () => {
-    console.log(`Server is up and running on port : ${PORT}`);
+    console.log(`server is up and running on port :  http://localhost:${PORT}`);
 });
--- utils/validatePassword.js
+++ utils/validatePassword.js
@@ -1,12 +1,3 @@
-/*
-    This function is used to validate the password.
-    - The password must be between 6 to 16 characters long.
-    - The password must contain at least one numeric digit.
-    - The password must contain at least one uppercase letter.
-    - The password must contain at least one special character.
-    - The password must not contain any whitespace.
-*/
-
 const validatePassword = (password) => {
     const pattern =
         /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,16}$/;
